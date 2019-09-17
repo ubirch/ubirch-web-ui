19bc28322361557ad89d0523c60112d341d41bf8
dieAte <beate.fiss@cameo.io>
2019-09-17 14:38:27 +0200

    keycloak script extended to also define the web app url

--- keycloak/realm-templates/create-new-tenant-realm4import.sh
+++ keycloak/realm-templates/create-new-tenant-realm4import.sh
@@ -2,22 +2,25 @@
 
 realmname=$1
 serverurl=$2
+webappurl=$3
 
-if [[ $# -ne 2 ]]
+if [[ $# -ne 3 ]]
 then
       echo "Please add required parameters:"
       echo "  1. the name of the tenant realm"
       echo "  2. the server url with protocol and port if needed"
-      echo "EXAMPLE:"
+      echo "  3. the url where the web ui will be accessible with protocol and port if needed"
+      echo "EXAMPLES:"
       echo ""
-      echo "./create-new-tenant-realm4import.sh testmandant-realm http://localhost:8080"
+      echo "./create-new-tenant-realm4import.sh testmandant-realm http://localhost:8080 http://localhost.9101"
+      echo "./create-new-tenant-realm4import.sh ubirch-default-realm https://id.dev.ubirch.com https://console.dev.ubirch.com"
 else
       template4realm="TENANT_REALM_TEMPLATE-export.json"
       template4connector="TENANT_REALM_TEMPLATE-connector.json"
       importfile="$realmname-import.json"
       importConnectorfile="$realmname-connector-import.json"
 
-      sed '/"id": "TENANT_REALM_TEMPLATE"/! s/"id": ".*"/"id": ""/g' $template4realm | sed 's/"internalId": ".*"/"id": ""/g' | sed '/"id": ""/d' | sed "s#TENANT_REALM_TEMPLATE#$realmname#g" | sed "s#<SERVER_URL>#$serverurl#g" > $importfile
+      sed '/"id": "TENANT_REALM_TEMPLATE"/! s/"id": ".*"/"id": ""/g' $template4realm | sed 's/"internalId": ".*"/"id": ""/g' | sed '/"id": ""/d' | sed "s#TENANT_REALM_TEMPLATE#$realmname#g" | sed "s#<SERVER_URL>#$serverurl#g" | sed "s#<WEBAPP_URL>#$webappurl#g" > $importfile
 
       echo "create a new realm from $importfile in your keycloak instance"
 
