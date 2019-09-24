0d5f5f996f852b5248923b4bcdc6bbe287601eaa
dieAte <beate.fiss@cameo.io>
2019-09-24 15:03:33 +0200

    Logout page displays the right logged in user; creation date currently not displayed on device details

--- src/app/tabs/logout/logout.page.ts
+++ src/app/tabs/logout/logout.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {KeycloakService} from 'keycloak-angular';
 import {HeaderActionButton} from '../../components/header/header-action-button';
 import {Location} from '@angular/common';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-logout',
@@ -12,7 +13,8 @@ export class LogoutPage implements OnInit {
 
   constructor(
       private keycloakService: KeycloakService,
-      private location: Location
+      private location: Location,
+      private userService: UserService
   ) { }
 
   actionButtons = [new HeaderActionButton({
@@ -21,9 +23,16 @@ export class LogoutPage implements OnInit {
     iconPath: 'assets/app-icons/back-button.svg',
     action: 'historyBack'
   })];
-  username = 'Max Mustermann';
+  username = '';
 
   ngOnInit() {
+    this.userService.observableAccountInfo.subscribe(accountInfo => {
+      if (accountInfo) {
+        this.username = accountInfo.user.toString();
+      } else {
+        this.userService.getAccountInfo().subscribe();
+      }
+    });
   }
 
   handleButtonClick(action: string) {
