ed89cb96e42f96698130960654dfc3510e7a01a6
dieAte <beate.fiss@cameo.io>
2019-09-24 14:37:03 +0200

    buttons corrected (label tag in button removed if added accidently)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import {KeycloakService} from 'keycloak-angular';
 import {environment} from '../environments/environment';
 
 @Component({
