03e544d1cf3afbf14668515c024108596b6d5c92
dieAte <beate.fiss@cameo.io>
2019-09-30 10:38:34 +0200

    version 0.1.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,7 +47,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      console.log('version 0.1.2');
+      console.log('version 0.1.3');
     });
   }
 }
