7dba2f0488586cbc1ad69e1df284a47629db578f
dieAte <beate.fiss@cameo.io>
2019-10-23 15:57:11 +0200

    UP-955: get and show pubkeylist (not styled...)

--- src/app/tabs/devices/device-details/tabs/device-pubkeys/device-pubkeys.page.ts
+++ src/app/tabs/devices/device-details/tabs/device-pubkeys/device-pubkeys.page.ts
@@ -11,13 +11,26 @@ import {PubKeyInfo} from '../../../../../models/pub-key-info';
 })
 export class DevicePubkeysPage implements OnInit {
 
-  loadedDevice: Device;
-  pubKeyList: PubKeyInfo[];
+    loadedDevice: Device;
+    public columnNames: any;
+    public pubKeyList: PubKeyInfo[];
 
   constructor(
       private deviceService: DeviceService,
       private keyService: KeyService
-  ) { }
+  ) {
+      // Define the columns for the data table
+      // (based off the names of the keys in the JSON file)
+      this.columnNames = [
+          { prop: 'pubKey', name: 'öffentlicher Schlüssel' },
+          { prop: 'pubKeyId', name: 'SchlüsselID' },
+          { prop: 'algorithm', name: 'Algorithmus' },
+          { prop: 'created', name: 'erzeugt' },
+          { prop: 'validNotBefore', name: 'gültig ab' },
+          { prop: 'validNotAfter', name: 'ungültig ab' },
+          { prop: 'signed', name: 'signiert' }
+      ];
+  }
 
   ngOnInit() {
     this.deviceService.observableCurrentDevice
@@ -27,7 +40,8 @@ export class DevicePubkeysPage implements OnInit {
               if (this.loadedDevice && this.loadedDevice.hwDeviceId) {
                 // load pubKeys
                 this.keyService.getPubKeysOfThing(this.loadedDevice.hwDeviceId)
-                    .subscribe( pubKeyList => this.pubKeyList = pubKeyList );
+                    .subscribe( pubKeyList =>
+                        this.pubKeyList = pubKeyList && pubKeyList.length > 0 ? pubKeyList : undefined );
               }
             }
         );
