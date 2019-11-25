be30189c59dcda705199421376ab8f0986f7ee21
dieAte <beate.fiss@cameo.io>
2019-11-25 17:27:59 +0100

    UP-1082: Documented in logout-Guard, that getAccountInfo is called by header component

--- src/app/auth/enter-on-dashboard.guard.ts
+++ src/app/auth/enter-on-dashboard.guard.ts
@@ -5,6 +5,11 @@ import {UserService} from '../services/user.service';
 @Injectable({
   providedIn: 'root',
 })
+/**
+ * AuthGuard for logout page: if user logs in after logout keycloak remembers last url => user would enter console on logout page!
+ * With this guard the logout page can be entered after userInfo has been accessed successfully.
+ * Attention: userInfo is fetched in userService.getAccountInfo, which is called by header component.
+ */
 export class EnterOnDashboardGuard implements CanActivate {
   constructor(
     private userService: UserService,
