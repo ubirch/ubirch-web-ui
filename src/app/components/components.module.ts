f301b49ef5ea05d5536ab355a1ea6bd85772ed4a
dieAte <beate.fiss@indarium.de>
2020-09-09 22:05:58 +0200

    UP-2058: added translations to verification header; removed unused component tag-chips-list-input.component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,36 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {FormsModule} from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
-import { FileInputComponent } from './file-input/file-input.component';
+import {FileInputComponent} from './file-input/file-input.component';
 import {VerificationHeaderComponent} from './header/verification-header/verification-header.component';
 import {LoggedInUserComponent} from './logged-in-user/logged-in-user.component';
-import {TagChipsListInputComponent} from './tag-chips-list-input/tag-chips-list-input.component';
 import {TranslateModule} from '@ngx-translate/core';
-
 
 
 @NgModule({
   declarations: [
-      HeaderComponent,
-      VerificationHeaderComponent,
-      LoggedInUserComponent,
-      FileInputComponent,
-      TagChipsListInputComponent
+    HeaderComponent,
+    VerificationHeaderComponent,
+    LoggedInUserComponent,
+    FileInputComponent,
   ],
-    imports: [
-      CommonModule,
-      FormsModule,
-      IonicModule,
-      TranslateModule
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    TranslateModule
   ],
   exports: [
-      HeaderComponent,
-      VerificationHeaderComponent,
-      LoggedInUserComponent,
-      FileInputComponent,
-      TagChipsListInputComponent
+    HeaderComponent,
+    VerificationHeaderComponent,
+    LoggedInUserComponent,
+    FileInputComponent,
   ]
 })
-export class ComponentsModule { }
+export class ComponentsModule {
+}
