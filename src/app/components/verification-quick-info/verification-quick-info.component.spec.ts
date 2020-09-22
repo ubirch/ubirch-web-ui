9e7f99d41c08d473453a49e88ab8d4f6b1731e1e
paul.lengert <paullengert0510@gmail.com>
2020-09-22 15:48:31 +0200

    UNG-401: added Icons, changed status name

--- src/app/components/verification-quick-info/verification-quick-info.component.spec.ts
+++ src/app/components/verification-quick-info/verification-quick-info.component.spec.ts
@@ -1,27 +1,27 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { VerificationQuickInfoComponent } from './verification-quick-info.component';
+import {VerificationQuickInfoComponent} from './verification-quick-info.component';
 
 describe('VerificationQuickInfoComponent', () => {
-  let component: VerificationQuickInfoComponent;
-  let fixture: ComponentFixture<VerificationQuickInfoComponent>;
+    let component: VerificationQuickInfoComponent;
+    let fixture: ComponentFixture<VerificationQuickInfoComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ VerificationQuickInfoComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [VerificationQuickInfoComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(VerificationQuickInfoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(VerificationQuickInfoComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/components/verification-quick-info/verification-quick-info.component.ts
+++ src/app/components/verification-quick-info/verification-quick-info.component.ts
@@ -1,97 +1,155 @@
 import {Component, Input, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import { IUbirchBlockchain } from 'src/app/models/iubirch-blockchain';
-import { IUbirchBlockchainNet } from 'src/app/models/iubirch-blockchain-net';
-import { Upp } from 'src/app/models/upp';
-import { IUbirchAnchorObject } from 'widgets/verification/models';
-import {TrustService, VERIFICATION_STATE} from '../../services/trust.service';
-//import BlockchainSettings from '../../../../resources/blockchain-settings.json'
-import { environment } from 'resources/clients/example-resources/environments/environment.local';
-
+import {IUbirchBlockchain} from 'src/app/models/iubirch-blockchain';
+import {IUbirchBlockchainNet} from 'src/app/models/iubirch-blockchain-net';
+import {IUbirchAnchorObject} from 'widgets/verification/models';
+import {TrustService} from '../../services/trust.service';
+import {Upp} from '../../models/upp';
 
 @Component({
-  selector: 'ubirch-verification-quick-info',
-  templateUrl: './verification-quick-info.component.html',
-  styleUrls: ['./verification-quick-info.component.scss'],
+    selector: 'ubirch-verification-quick-info',
+    templateUrl: './verification-quick-info.component.html',
+    styleUrls: ['./verification-quick-info.component.scss'],
 })
 export class VerificationQuickInfoComponent implements OnInit {
-  @ViewChild('NO_HASH', {static: true}) NO_HASH: TemplateRef<any>;
-  @ViewChild('HASH_INSERTED_UNVERIFIED', {static: true}) HASH_INSERTED_UNVERIFIED: TemplateRef<any>;
-  @ViewChild('PENDING', {static: true}) PENDING: TemplateRef<any>;
-  @ViewChild('HASH_VERIFIED', {static: true}) HASH_VERIFIED: TemplateRef<any>;
-  @ViewChild('HASH_VERIFICATION_FAILED', {static: true}) HASH_VERIFICATION_FAILED: TemplateRef<any>;
-  @ViewChild('HASH_VERIFICATION_ERROR', {static: true}) HASH_VERIFICATION_ERROR: TemplateRef<any>;
-  @ViewChild('SERVICE_CURRENTLY_UNAVAILABLE', {static: true}) SERVICE_CURRENTLY_UNAVAILABLE: TemplateRef<any>;
-  
-  constructor(private trustService: TrustService) { }
+    @ViewChild('NO_HASH', {static: true}) NO_HASH: TemplateRef<any>;
+    @ViewChild('HASH_INSERTED_UNVERIFIED', {static: true}) HASH_INSERTED_UNVERIFIED: TemplateRef<any>;
+    @ViewChild('PENDING', {static: true}) PENDING: TemplateRef<any>;
+    @ViewChild('HASH_VERIFIED', {static: true}) HASH_VERIFIED: TemplateRef<any>;
+    @ViewChild('HASH_VERIFICATION_FAILED', {static: true}) HASH_VERIFICATION_FAILED: TemplateRef<any>;
+    @ViewChild('HASH_VERIFICATION_ERROR', {static: true}) HASH_VERIFICATION_ERROR: TemplateRef<any>;
+    @ViewChild('SERVICE_CURRENTLY_UNAVAILABLE', {static: true}) SERVICE_CURRENTLY_UNAVAILABLE: TemplateRef<any>;
+    // hashState = VERIFICATION_STATE.NO_HASH;
+    hash = '';
+    blockchains: any;
+    anchors = [];
+    verificationState: string;
+    verificationDisplay = {
+        iVerIconSrc: '',
+        iClass: '',
+        aVerIconSrc: '',
+        aClass: ''
+    };
+    iconPath = TrustService.BlockchainIconsPath;
 
-  hashState = VERIFICATION_STATE.NO_HASH;
-  hash = '';
-  upp: any;
-  anchors = [];
-  
-
-  
-
-  ngOnInit() {
-    this.trustService.observableVerificationState.subscribe(verificationState => this.hashState = verificationState); 
-    this.trustService.observableHash.subscribe(hash => this.hash = hash); 
-    this.trustService.observableUPP.subscribe(upp => this.upp = upp)  
-    console.log('upp')   
-    if(this.upp){
-      this.showAnchors(this.upp.jsonInput.anchors.upper_blockchains)
-    }    
-    console.log(this.anchors)
-  }
-
-  showAnchors(bloxTX) {
-    if (!bloxTX) {
-      console.log('keine Blockchains')
-      return;
+    constructor(private trustService: TrustService) {
     }
 
-    bloxTX.forEach((item) => {
-      console.log(item)
-      if (!item || !item.properties) {
-        return;
-      } else {
-        const blockchain: string = item.properties.blockchain;
-        console.log('blockchain')
-        console.log(blockchain)
-   
-        const networkType: string = item.properties.network_type;
-        console.log('networkType')
-        console.log(networkType)
-    
+    @Input()
+    public set hashState(state: string) {
+        this.verificationState = state;
+        console.log(this.verificationState);
+    }
 
-        if (!blockchain || !networkType) {
-           return;
+    @Input()
+    public set upp(upp: Upp) {
+        this.blockchains = upp.anchors.upperBlockChains;
+        console.log(upp);
+    }
+
+    ngOnInit() {
+        this.showAnchors(this.blockchains);
+        this.setIIcon();
+        this.setAIcon();
+
+
+    }
+
+    showAnchors(bloxTX) {
+        if (!bloxTX) {
+
+            return;
         }
 
-        const blox: IUbirchBlockchain = TrustService.BlockchainSettings[blockchain];  
-        console.log('blox')
-        console.log(blox) 
+        bloxTX.forEach((item) => {
 
-        if (!blox) {
-          return;
+            if (!item || !item) {
+                return;
+            } else {
+                const blockchain: string = item.blockchain;
+
+
+                const networkType: string = item.networkType;
+
+
+                if (!blockchain || !networkType) {
+                    return;
+                }
+
+                const blox: IUbirchBlockchain = TrustService.BlockchainSettings[blockchain];
+
+
+                if (!blox) {
+                    return;
+                }
+
+                const bloxTXData: IUbirchBlockchainNet = blox.explorerUrl[networkType];
+                const anchor: IUbirchAnchorObject = {href: undefined, icon: '', target: '', title: ''};
+
+                if (bloxTXData.url) {
+                    anchor.href = bloxTXData.url.toString() + item.txid;
+                }
+
+                anchor.title = bloxTX.network_info ? bloxTX.network_info : bloxTX.blockchain;
+                anchor.target = '_blanc';
+
+                if (blox.nodeIcon) {
+                    anchor.icon = this.iconPath + blox.nodeIcon;
+
+                }
+                this.anchors.push(anchor);
+            }
+        });
+    }
+
+    setIIcon() {
+        switch (this.verificationState) {
+            case 'PENDING':
+                this.verificationDisplay.iVerIconSrc = this.iconPath + 'hourglass-outline.svg';
+                this.verificationDisplay.iClass = 'notAvailable';
+                break;
+            case 'HASH_INSERTED_UNVERIFIED':
+                this.verificationDisplay.iVerIconSrc = this.iconPath + 'hourglass-outline.svg';
+                this.verificationDisplay.iClass = 'notAvailable';
+                break;
+            case 'HASH_VERIFICATION_FAILED':
+                this.verificationDisplay.iVerIconSrc = this.iconPath + 'warning-outline.svg';
+                this.verificationDisplay.iClass = 'fail';
+                break;
+            case 'HASH_VERIFIED':
+                this.verificationDisplay.iVerIconSrc = this.iconPath + 'shield-checkmark-outline.svg';
+                this.verificationDisplay.iClass = 'success';
+                break;
+            case 'HASH_VERIFICATION_ERROR':
+                this.verificationDisplay.iVerIconSrc = this.iconPath + 'warning-outline.svg';
+                this.verificationDisplay.iClass = 'fail';
+                break;
+            case 'SERVICE_CURRENTLY_UNAVAILABLE':
+                this.verificationDisplay.iVerIconSrc = this.iconPath + 'hourglass-outline.svg';
+                this.verificationDisplay.iClass = 'notAvailable';
+                break;
         }
+    }
 
-        const bloxTXData: IUbirchBlockchainNet = blox.explorerUrl[networkType];
-        const anchor: IUbirchAnchorObject = {href: undefined, icon: '', target: '', title: ''};
-
-        if (bloxTXData.url) {
-          anchor.href = bloxTXData.url.toString() + item.properties.txid;
+    setAIcon() {
+        switch (this.verificationDisplay.iClass) {
+            case 'success':
+                if (this.anchors === []) {
+                    this.verificationDisplay.aClass = 'notAvailable';
+                    this.verificationDisplay.aVerIconSrc = this.iconPath + 'hourglass-outline.svg';
+                } else {
+                    this.verificationDisplay.aClass = 'success';
+                    this.verificationDisplay.aVerIconSrc = this.iconPath + 'shield-checkmark-outline.svg';
+                }
+                break;
+            case 'notAvailable' :
+                this.verificationDisplay.aClass = 'notAvailable';
+                this.verificationDisplay.aVerIconSrc = this.iconPath + 'hourglass-outline.svg';
+                break;
+            case 'fail' :
+                this.verificationDisplay.aClass = 'fail';
+                this.verificationDisplay.aVerIconSrc = this.iconPath + 'warning-outline.svg';
+                break;
         }
-
-        anchor.title = bloxTX.network_info ? bloxTX.network_info : bloxTX.blockchain;
-        anchor.target = '_blanc';
-
-        if (blox.nodeIcon) {
-          anchor.icon = TrustService.BlockchainIconsPath + blox.nodeIcon;
-          console.log(anchor.icon)
-        }
-        this.anchors.push(anchor);
-      }
-    });   
-  }
+    }
 
 }
