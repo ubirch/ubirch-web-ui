c4c9003d24847755f4ddc97ecb51324d38dce697
dieAte <beate.fiss@indarium.de>
2020-10-01 12:48:50 +0200

    UNG-525: created new route

---
+++ src/app/tabs/account-profile/account-profile.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AccountProfilePage } from './account-profile.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AccountProfilePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [AccountProfilePage]
+})
+export class AccountProfilePageModule {}
